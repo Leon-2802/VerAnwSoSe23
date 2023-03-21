b64346151507aae4ee7f42ff9c4b2c0b005c81cf
Leon Gobbert <leon.gobbert@hs-furtwangen.de>
2023-03-21 13:04:53 +0100

    WIP Aufg Serivice

---
+++ Verteilte_Anwendungen/src/Services_01_TimeService/TimeService.java
@@ -0,0 +1,52 @@
+package Services_01_TimeService;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeService {
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(7885);
+		while (true) {
+			
+			try {
+				Socket socket = serverSocket.accept();
+				
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				writer.write("connected!");
+				writer.newLine();
+				writer.flush();
+				
+				
+				while (true) {
+					String input = reader.readLine();
+					
+					switch (input) {
+					case "date":
+						writer.write(Clock.date());
+						writer.newLine();
+						writer.flush();
+						break;
+					case "time":
+						writer.write(Clock.time());
+						writer.newLine();
+						writer.flush();
+						break;
+					default:
+						serverSocket.close();
+						break;
+					}
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+
+}
