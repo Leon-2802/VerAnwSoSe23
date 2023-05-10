a99f1c566b388306f19ab387cbba02837978738a
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2023-05-10 21:36:04 +0200

    HTTPS-Client done

--- Verteilte_Anwendungen/src/HTTPS_client/SimpleClient.java
+++ Verteilte_Anwendungen/src/HTTPS_client/SimpleClient.java
@@ -16,7 +16,8 @@ public class SimpleClient {
 	public static void main(String[] args) {
 		try {
 //			get("https://www.bundestag.de/presse");
-			System.out.println(urlExists("https://www.bundestag.de/presse"));
+//			System.out.println(urlExists("https://www.bundestag.de/presse"));
+			System.out.println(getContent("https://www.bundestag.de/presse"));
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -24,22 +25,8 @@ public class SimpleClient {
 		}
 	}
 	
-	public static void get(String urlString) throws UnknownHostException, IOException {
-		URL url = new URL(urlString);
-		int port = url.getPort() == -1 ? url.getDefaultPort() : url.getPort();
-		
-		SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-		SSLSocket socket = (SSLSocket) factory.createSocket(url.getHost(), port);
-		
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		
-		writer.write("GET " + url.getPath() + " HTTP/1.0");
-		writer.newLine();
-		writer.write("Host: " + url.getHost());
-		writer.newLine();
-		writer.newLine();
-		writer.flush();
+	public static void get(String s) throws IOException {
+		BufferedReader reader = connectToURL(s);
 		
 		String result = reader.readLine();
 		
@@ -49,26 +36,45 @@ public class SimpleClient {
 		}
 	}
 	
-	public static boolean urlExists(String s) throws UnknownHostException, IOException {
+	public static boolean urlExists(String s) throws IOException {
+		BufferedReader reader = connectToURL(s);
+		
+		String result = reader.readLine();
+		return (result.contains("200"));
+	}
+
+	public static String getContent(String s) throws IOException {
+		StringBuilder result = new StringBuilder();
+		BufferedReader reader = connectToURL(s);
+
+		String line = reader.readLine();
+
+		while(line != null) {
+			result.append(line + "\n");
+			line = reader.readLine();
+		}
+
+		return result.toString();
+	}
+
+	private static BufferedReader connectToURL(String s) throws IOException {
 		URL url = new URL(s);
 		int port = url.getPort() == -1 ? url.getDefaultPort() : url.getPort();
-		
+
 		SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 		SSLSocket socket = (SSLSocket) factory.createSocket(url.getHost(), port);
-		
+
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		
+
 		writer.write("GET " + url.getPath() + " HTTP/1.0");
 		writer.newLine();
 		writer.write("Host: " + url.getHost());
 		writer.newLine();
 		writer.newLine();
 		writer.flush();
-		
-		String result = reader.readLine();
-		
-		return (result.contains("200"));
+
+		return reader;
 	}
 
 }
