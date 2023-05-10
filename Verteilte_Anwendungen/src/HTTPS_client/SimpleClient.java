c5d9f5764358ca75bc693c64308b1a684a7f193f
Leon Gobbert <leon.gobbert@hs-furtwangen.de>
2023-05-10 20:49:23 +0200

    SimpleClient WIP

---
+++ Verteilte_Anwendungen/src/HTTPS_client/SimpleClient.java
@@ -0,0 +1,74 @@
+package HTTPS_client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SimpleClient {
+
+	public static void main(String[] args) {
+		try {
+//			get("https://www.bundestag.de/presse");
+			System.out.println(urlExists("https://www.bundestag.de/presse"));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void get(String urlString) throws UnknownHostException, IOException {
+		URL url = new URL(urlString);
+		int port = url.getPort() == -1 ? url.getDefaultPort() : url.getPort();
+		
+		SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		SSLSocket socket = (SSLSocket) factory.createSocket(url.getHost(), port);
+		
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		
+		writer.write("GET " + url.getPath() + " HTTP/1.0");
+		writer.newLine();
+		writer.write("Host: " + url.getHost());
+		writer.newLine();
+		writer.newLine();
+		writer.flush();
+		
+		String result = reader.readLine();
+		
+		while(result != null) {
+			System.out.println(result);
+			result = reader.readLine();
+		}
+	}
+	
+	public static boolean urlExists(String s) throws UnknownHostException, IOException {
+		URL url = new URL(s);
+		int port = url.getPort() == -1 ? url.getDefaultPort() : url.getPort();
+		
+		SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		SSLSocket socket = (SSLSocket) factory.createSocket(url.getHost(), port);
+		
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		
+		writer.write("GET " + url.getPath() + " HTTP/1.0");
+		writer.newLine();
+		writer.write("Host: " + url.getHost());
+		writer.newLine();
+		writer.newLine();
+		writer.flush();
+		
+		String result = reader.readLine();
+		
+		return (result.contains("200"));
+	}
+
+}
